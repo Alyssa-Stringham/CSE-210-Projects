3da959abd54d2e5364966410439b54afc92ea780
Alyssa-Stringham <astringham@byupathway.edu>
2026-01-10 11:20:44 -0700

    Week 1 Assignment 3 Complete

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        //Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        //Console.Write("What is the magic number? ");
+        //string numberString = Console.ReadLine();
+        //int number = int.Parse(numberString);
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,101);
+        int guess = -1;
+        do
+        { 
+        Console.Write("What is your guess? ");
+        string guessString = Console.ReadLine();
+        guess = int.Parse(guessString);   
+            if (number != guess)
+            {
+                if (number < guess)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("Higher");
+                }
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        } while (guess != number);
     }
 }
