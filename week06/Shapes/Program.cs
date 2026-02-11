345c586e2f4850abe9801a3257777f1a7141af65
Alyssa-Stringham <astringham@byupathway.edu>
2026-02-10 17:05:01 -0700

    Week 6 Learning Activity

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Drawing;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        SquareShape square = new SquareShape("Red", 5);
+        //string squareColor = square.GetColor();
+        //double squareArea = square.GetArea();
+        //Console.WriteLine($"{squareColor}, {squareArea}");
+
+        RectangleShape rectangle = new RectangleShape("Green", 4, 6);
+        //string rectangleColor = rectangle.GetColor();
+        //double rectangleArea = rectangle.GetArea();
+        //Console.WriteLine($"{rectangleColor}, {rectangleArea}");        
+        
+        CircleShape circle = new CircleShape("Blue", 3);
+        //string circleColor = circle.GetColor();
+        //double circleArea = circle.GetArea();
+        //Console.WriteLine($"{circleColor}, {circleArea}");        
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            double area = shape.GetArea();
+            string color = shape.GetColor();
+            Console.WriteLine($"The {color} shape has an area of {area}");
+        }
     }
 }
---
+++ week06/Shapes/SquareShape.cs
@@ -0,0 +1,14 @@
+using System.Reflection.Metadata;
+
+public class SquareShape : Shape
+{
+    private double _side; 
+    public SquareShape(string color, double side) : base(color)
+    {
+        _side = side; 
+    }
+    public override double GetArea()
+    {
+        return _side * _side;
+    }
+}
